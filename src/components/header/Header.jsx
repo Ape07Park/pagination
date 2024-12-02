0d235d283e810bba0eaacd2db1ab99cf2b4ed15a
SUNGMIN PARK <ape072399@gmail.com>
2024-12-02 18:26:59 +0900

    헤더 작업 중

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Header() {
 
+    const navigate = useNavigate();
+
+    const handleGoList = () => {
+        navigate('/list');
+    }
+    
     return (
         <>
+        <h1>
+            한국교원대 근현대사 서비스
+        </h1>
+
+        <button onClick={handleGoList}>리스트로 이동</button>
+
         </>
     )
 }
