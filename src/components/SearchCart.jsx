4a7f329efc66d729e2ac8c391871fe5f78967d29
SUNGMIN PARK <ape072399@gmail.com>
2024-12-05 17:19:58 +0900

    검색 모달창 구현 중

--- src/components/SearchCart.jsx
+++ src/components/SearchCart.jsx
@@ -1,47 +1,39 @@
 import { useRecoilValue } from "recoil";
 import { selectedTitle } from "../recoil/selectedTitle";
+import SearchModal from "../pages/SearchModal";
+import { useState } from "react";
 
 export default function SearchCart() {
 
+    const titleList = useRecoilValue(selectedTitle);
+    const [modalOpen, setModalOpen] = useState(false);
 
-   const titleList = useRecoilValue(selectedTitle);
+    const handleSearch = () => {
+        // 검색 시 모달 나오면서 검색 장바구니에 있던 것들을 모달창에 띄우기
+        setModalOpen(true)
+    }
 
-    // const handleCheckboxChange = (data) => {
-    //     const currentSelected = [...selectedItems];
-    //     const dataId = data.id;
-
-    //     const existingIndex = currentSelected.findIndex(item => item.id === dataId);
-
-    //     if (existingIndex >= 0) {
-
-    //         currentSelected.splice(existingIndex, 1);
-    //     } else {
-    //         let dataObj = {
-    //             id: dataId,
-    //             title: data.title
-    //         }
-    //         currentSelected.push(dataObj);
-    //     }
-    //     setSelectedItems(currentSelected);
-    // };
-
-    return(
+    return (
         <>
-        <h4 style={{textAlign:'center'}}>
-            검색어 장바구니
-        </h4>
-        <div>
-            개수: {titleList.length}
-        </div>
-        <ul>
+            <h4 style={{ textAlign: 'center' }}>
+                검색어 장바구니
+            </h4>
+            <div>
+                개수: {titleList.length}
+            </div>
+            <ul>
                 {titleList.map((title, index) => (
                     <li key={index}>{title.title}</li>
                 ))}
             </ul>
-        <div>
+            <div>
 
-        <button>검색하기(모달나옴)</button>
-        </div>
+                <button onClick={() => handleSearch}>검색하기(모달나옴)</button>
+            </div>
+
+            <SearchModal
+            dataList={titleList}
+            />
         </>
     );
 }
--- src/pages/SearchModal.jsx
+++ src/pages/SearchModal.jsx
@@ -1,8 +1,21 @@
-export default function SearchModal() {
+export default function SearchModal({dataList}) {
 
     return(
         <>
-        
+        <h1>나는 모달</h1>
+
+
+
+        <ul>
+            {dataList.map((item) => (
+                <li 
+                key={item.id}>
+                    title: {item.title},
+                    content: {item.body}
+                </li>
+            ))}
+
+        </ul>
         </>
     );
 }
