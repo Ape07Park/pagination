8e8da1ae386ce0481cba2b7397bf3c1faf9ee43a
SUNGMIN PARK <ape072399@gmail.com>
2024-12-05 18:06:17 +0900

    모달 완성

--- src/components/SearchCart.jsx
+++ src/components/SearchCart.jsx
@@ -4,14 +4,13 @@ import SearchModal from "../pages/SearchModal";
 import { useState } from "react";
 
 export default function SearchCart() {
-
     const titleList = useRecoilValue(selectedTitle);
     const [modalOpen, setModalOpen] = useState(false);
 
     const handleSearch = () => {
         // 검색 시 모달 나오면서 검색 장바구니에 있던 것들을 모달창에 띄우기
-        setModalOpen(true)
-    }
+        setModalOpen(true);
+    };
 
     return (
         <>
@@ -27,12 +26,12 @@ export default function SearchCart() {
                 ))}
             </ul>
             <div>
-
-                <button onClick={() => handleSearch}>검색하기(모달나옴)</button>
+                <button onClick={handleSearch}>검색하기(모달나옴)</button>
             </div>
 
             <SearchModal
-            dataList={titleList}
+                dataList={titleList}
+                isOpen={modalOpen}
             />
         </>
     );
--- src/pages/SearchModal.jsx
+++ src/pages/SearchModal.jsx
@@ -1,21 +1,16 @@
-export default function SearchModal({dataList}) {
-
-    return(
+export default function SearchModal({ dataList, isOpen }) {
+    return (
         <>
-        <h1>나는 모달</h1>
-
-
-
-        <ul>
-            {dataList.map((item) => (
-                <li 
-                key={item.id}>
-                    title: {item.title},
-                    content: {item.body}
-                </li>
-            ))}
-
-        </ul>
+            <h1>나는 모달</h1>
+            {isOpen && (
+                <ul>
+                    {dataList.map((item) => (
+                        <li key={item.id}>
+                            title: {item.title}, content: {item.body}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </>
     );
 }
